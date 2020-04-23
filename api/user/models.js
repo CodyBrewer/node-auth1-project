921e990bd5cb99f29706a9454d1132db7d6aaec0
CodyBrewer <codybrewerdev@gmail.com>
2020-04-22 21:37:12 -0600

    database migrations and seeds ran

---
+++ api/user/models.js
@@ -0,0 +1,23 @@
+const db = require('../../data/dbConfig.js')
+
+const findAll = () => {};
+
+const findById = (id) => {}:
+
+const findByName = (name) => {};
+
+const insert = (user) => {};
+
+const remove = (id) => {};
+
+const update = (id, changes) => {};
+
+module.exports = {
+    findAll,
+    findById,
+    findByName,
+    insert, 
+    remove,
+    update,
+}
+
